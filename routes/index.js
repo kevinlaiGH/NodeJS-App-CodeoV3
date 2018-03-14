92b9b896f75da2b84c6582b8d76541878caf2eab
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-14 14:45:48 +1100

    sending transactional emails doesnt work

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
+//mailing setting
+var nodemailer = require('nodemailer');
+var config = require('../config');
+var transporter = nodemailer.createTransport(config.mailer);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Codeo - a collaborative platform for pair-programming' });
+  res.render('index', { title: '🚀Codeo - a collaborative platform for pair-programming' });
 });
 
 /* creating a new route for about */
 router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'Codeo - a collaborative platform for pair-programming' });
+  res.render('about', { title: '🚀Codeo - a collaborative platform for pair-programming' });
 });
 
 /* creating a new route for contact*/
 router.route('/contact')
   .get(function (req, res, next) {
-    res.render('contact', { title: 'Codeo - a collaborative platform for pair-programming' });
+    res.render('contact', { title: '🚀Codeo - a collaborative platform for pair-programming' });
   })
   /* creating a form to submit for POST req*/
   .post(function (req, res, next) {
@@ -25,14 +29,26 @@ router.route('/contact')
 
     if (errors) {
       res.render('contact', {
-        title: 'Codeo - a collaborative platform for pair-programming',
+        title: '🚀Codeo - a collaborative platform for pair-programming',
         name: req.body.name,
         email: req.body.email,
         message: req.body.message,
         errorMessages: errors
       });
     } else {
-      res.render('thank', { title: 'Codeo - a collaborative platform for pair-programming' });
+      var mailOptions = {
+        from: 'Codeo <no-reply@codeo.com>',
+        to: 'klonto60k@gmail.com',
+        subject: 'You got a new message from visitor 🤩🤩',
+        text: req.body.message
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        res.render('thank', { title: '🚀Codeo - a collaborative platform for pair-programming' });
+      });
     }
   });
 
