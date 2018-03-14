fd3f72481b3384986be03bd79a35dd66906d545e
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-14 11:29:12 +1100

    contact page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,14 @@ router.get('/about', function (req, res, next) {
   res.render('about', { title: 'Codeo - a collaborative platform for pair-programming' });
 });
 
+/* creating a new route for contact*/
+router.route('/contact')
+  .get(function (req, res, next) {
+    res.render('contact', { title: 'Codeo - a collaborative platform for pair-programming' });
+  })
+  /* creating a form to submit for POST req*/
+  .post(function (req, res, next) {
+    res.render('thank you', { title: 'Codeo - a collaborative platform for pair-programming' });
+  });
+
 module.exports = router;
