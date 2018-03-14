29d3d4906cf79438c09a94071cfd9aef6b0e7fc9
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-14 11:01:29 +1100

    creating static page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Codeo - a collaborative platform for pair-programming' });
+});
+
+/* creating a new route for about */
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'Codeo - a collaborative platform for pair-programming' });
 });
 
 module.exports = router;
