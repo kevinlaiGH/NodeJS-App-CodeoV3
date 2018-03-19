4034ed74c08bcb27a575fa8a1108ac05006d95ae
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-19 12:23:07 +1100

    login and registration page

--- routes/index.js
+++ routes/index.js
@@ -51,5 +51,11 @@ router.route('/contact')
       });
     }
   });
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Login your account' });
+});
 
+router.get('/register', function (req, res, next) {
+  res.render('register', { title: 'Register a new account' });
+});
 module.exports = router;
